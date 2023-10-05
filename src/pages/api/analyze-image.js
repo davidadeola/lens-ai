baaf96b2dba0aba64659642c19ba34d722f9506c
davidadeola <davidolorunsesan15@gmail.com>
2023-10-05 05:43:56 +0100

    wip - setup cloudinary upload, and google vision api

---
+++ src/pages/api/analyze-image.js
@@ -0,0 +1,48 @@
+import cloudinary from "cloudinary";
+import vision from "@google-cloud/vision";
+// Configure Cloudinary
+cloudinary.v2.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_API_SECRET,
+});
+
+const visionClient = new vision.ImageAnnotatorClient({
+  keyFilename: "./credentials/my-credentials.json",
+});
+
+const analyzeImage = async (imageUrl) => {
+  try {
+    // Upload the image to cloudinary
+    const cloudinaryResponse = await cloudinary.v2.uploader.upload(imageUrl);
+
+    // Analyze the image using Google Vision API
+    const [result] = await visionClient.labelDetection(
+      cloudinaryResponse.secure_url
+    );
+    const labels = result.labelAnnotations;
+    console.log(labels);
+    return labels;
+  } catch (error) {
+    console.error("Error analyzing image:", error);
+    throw new Error("An error occurred while analyzing the image");
+  }
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method Not Allowed" });
+    return;
+  }
+
+  const { imageUrl } = req.body;
+
+  try {
+    const labels = await analyzeImage(imageUrl);
+    console.log(labels);
+    res.status(200).json({ labels });
+  } catch (error) {
+    res.status(500).json({ error: error.message || "An error occurred" });
+  }
+};
